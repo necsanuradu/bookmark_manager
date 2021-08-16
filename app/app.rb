c53a08f007f603406bb95c862d78bac53a8d8d61
Radu Necsanu <necsanu_radu@yahoo.com>
2021-08-16 14:57:51 +0100

    running server and rspec

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,12 @@
+#!/usr/bin/env ruby
 require "sinatra"
 require "sinatra/reloader"
 
-class Bookmark < Sinatra::base
+class Bookmark < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
   enable :sessions
+
+  run! if app_file ==$0
 end
